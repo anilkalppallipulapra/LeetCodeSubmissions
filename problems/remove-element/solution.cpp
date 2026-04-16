9d43b0256d012b5036350c632164f49f102a1fc3
anilkalppallipulapra <anilkalpallipulapra@gmail.com>
2026-04-16 15:11:08 +0530

    Implemented Remove Element using two-pointer technique:

---
+++ problems/remove-element/solution.cpp
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=27 lang=cpp
+ *
+ * [27] Remove Element
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k = 0; // index for elements not equal to val
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+};
+// @lc code=end
+
